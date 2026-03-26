7710daabaca762539e8b459c864421dbab83f177
sornersbooms <oscardanielyepez777@gmail.com>
2026-03-25 20:54:13 -0500

    🔌 Config: Support more MongoDB environment variable names

--- server/src/index.ts
+++ server/src/index.ts
@@ -56,7 +56,7 @@ app.use((req: any, res: any) => {
   res.sendFile(path.join(clientDistPath, 'index.html'));
 });
 
-const MONGODB_URI = process.env.MONGODB_URL || process.env.MONGODB_URI || 'mongodb://localhost:27017/obtenpalabras';
+const MONGODB_URI = process.env.MONGODB_URL || process.env.MONGO_URL || process.env.MONGODB_URI || 'mongodb://localhost:27017/obtenpalabras';
 
 mongoose.connect(MONGODB_URI)
   .then(() => console.log('✅ Conectado a MongoDB SaaS'))
